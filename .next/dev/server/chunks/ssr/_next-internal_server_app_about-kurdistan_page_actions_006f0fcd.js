f32d05637122130c5eebb397f433c9dcd6a239a7
Raghav2003gaur <{{raghav27gaur@gmail.com}}>
2025-12-03 21:42:30 +0530

    Update .gitignore to exclude node_modules and .next build files

--- .next/dev/server/chunks/ssr/_next-internal_server_app_about-kurdistan_page_actions_006f0fcd.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/about-kurdistan/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_about-kurdistan_page_actions_006f0fcd.js.map
--- .next/dev/server/chunks/ssr/_next-internal_server_app_book-villa_page_actions_61d3e013.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/book-villa/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_book-villa_page_actions_61d3e013.js.map
